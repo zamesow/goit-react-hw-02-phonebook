710ff48478e0e633d243c5246b21d67bf034192e
zamesow <zamesow@gmail.com>
2021-01-22 20:33:23 +0200

    updete return in Contacts

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -7,7 +7,9 @@ function Contacts({ title, contactsProp }) {
       <h2>{title}</h2>
       <ul>
         {contactsProp.map(contact => {
-          <li key={contact.id}>{contact.name}</li>;
+          return (
+            <li key={contact.id}>{contact.name}</li>
+          );
         })}
       </ul>
     </div>
