38312e436daba9cf4d32261dfc7542677a859588
zamesow <zamesow@gmail.com>
2021-03-07 15:54:18 +0200

    update props in ContactList

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import mc from './ContactList.module.css';
 
-function ContactList({ contactsProp, onDeleteProp, children }) {
+function ContactList({ contacts, onDelete, children }) {
   return (
     <div className={mc.ContactList}>
       {children}
       <ul className={mc.ContactList__form}>
-        {contactsProp.map(({ id, number, name }) => {
+        {contacts.map(({ id, number, name }) => {
           return (
             <li key={id} className={mc.contact}>
               {name}: {number}
               <button
                 className={mc.deleteBtn}
                 type="button"
-                onClick={() => onDeleteProp(id)}
+                onClick={() => onDelete(id)}
               >
                 Delete
               </button>
